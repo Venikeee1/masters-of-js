2084b3de31860b2a350169da133b0f88beddd301
a.Vienik <alexandrVenik@gmail.com>
2021-04-15 22:05:46 +0300

    add module-12 lesson 2

--- module-12/src/index.js
+++ module-12/src/index.js
@@ -1,31 +1,5 @@
 import './styles.css';
 import './css/preloader.css';
 import './css/apartments.css';
-import apartmentService from './js/services/apartments.service';
-import preloaderFactory from './js/preloader';
-import apartmentsListTemplate from './templates/apartments-list.hbs';
-
-const preloader = preloaderFactory('.preloader');
-const apartmentsListRef = document.querySelector('.apartments-list');
-const refreshBtnRef = document.querySelector('.refresh');
-
-const renderApartmentsList = () => {
-  apartmentService
-    .fetchApartments()
-    .then(apartments => {
-      apartmentsListRef.innerHTML = apartmentsListTemplate(apartments);
-    })
-    .catch(error => {
-      apartmentsListRef.innerHTML = 'There was an error';
-    })
-    .finally(() => {
-      preloader.hide();
-    });
-};
-
-refreshBtnRef.addEventListener('click', () => {
-  preloader.show();
-  renderApartmentsList();
-});
-
-renderApartmentsList();
+import './css/add-apartment-form.css';
+import './js/apartments/';
